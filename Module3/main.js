5c70f9e2faff668ffb082e2956ace0402165942a
Dn12 <62524139+D-power14@users.noreply.github.com>
2020-08-06 22:26:01 -0400

    Usando js en 15 minutos con todo

--- Module3/main.js
+++ Module3/main.js
@@ -3,6 +3,15 @@ var x = "Hello World!";
 
 //Mostrar en consola web
 
-
 console.log(x);
 console.log("D-power!!!!")
+
+//el cuadro de alerta
+
+alert("HI? user283459344")
+
+//escribir en el viewport con js
+
+document.write("Hola mundo")
+
+//suma de caracteres y para espacios esto "  "
