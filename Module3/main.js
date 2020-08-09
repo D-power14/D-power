516c61021311c692ff0ba341ccf46523eef86d2f
Dn12 <62524139+D-power14@users.noreply.github.com>
2020-08-09 09:06:21 -0400

    Glosario casi-listo de 15 minutos

--- Module3/main.js
+++ Module3/main.js
@@ -1,10 +1,10 @@
+
 //donde declaro variables
 
 var x = "Hello World!"; 
 var y = "D-power!!!";
 var xy = x + "  " + y;
-var o = 4
-var u =2
+
 
 
 
@@ -19,7 +19,16 @@ document.write("Hola mundo")
 
 //suma de caracteres y para espacios esto "  "
 
+var o = 4
+var u = 2
 console.log(xy)
+console.log(o + u) //suma el 4 con el 2
+
+//Un for es soltar codigo si se cumple un requisito
+
+for (o = 0; o < 10; o++) {
+     document.write(" uwu ")
+}
 
 //ifelse, ejm es si "o" es mayor que "u" ejecutar console.log
 
@@ -40,7 +49,7 @@ a(" Hola!! 980");
 //Variables con multiples valores = array con un for que muestra todo los nombres y un forEach que es para buscar
 
 
-var nombres = ['victor','antonio','joaquin']
+var nombres = [' victor',' antonio',' joaquin']
 
 for ( i = 0; i < nombres.length; i++) {
      document.write(nombres[i] + '<br>')
@@ -53,4 +62,6 @@ nombres.forEach(function (nombre) {
 });
 
 //alert(nombres[0]);
+
 //el cuadro de alerta = alert("HI? user283459344")
+
